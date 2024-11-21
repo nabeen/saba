b41a2cccbf53ba15af9a7ef03e2ce29b4c93b507
nabeen <nabeen@users.noreply.github.com>
2024-11-22 01:47:18 +0900

    feat: cssom struct

---
+++ saba_core/src/renderer/css/cssom.rs
@@ -0,0 +1,87 @@
+use crate::alloc::string::ToString;
+use crate::renderer::css::token::{CssToken, CssTokenizer};
+use alloc::string::String;
+use alloc::vec::Vec;
+use core::iter::Peekable;
+
+#[derive(Debug, Clone)]
+pub struct CssParser {
+    t: Peekable<CssTokenizer>,
+}
+
+impl CssParser {
+    pub fn new(t: CssTokenizer) -> Self {
+        Self { t: t.peekable() }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct StyleSheet {
+    pub rules: Vec<QualifiedRule>,
+}
+
+impl StyleSheet {
+    pub fn new() -> Self {
+        Self { rules: Vec::new() }
+    }
+
+    pub fn set_rules(&mut self, rules: Vec<QualifiedRule>) {
+        self.rules = rules;
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct QualifiedRule {
+    pub selector: Selector,
+    pub declarations: Vec<Declaration>,
+}
+
+impl QualifiedRule {
+    pub fn new() -> Self {
+        Self {
+            selector: Selector::TypeSelector("".to_string()),
+            declarations: Vec::new(),
+        }
+    }
+
+    pub fn set_selector(&mut self, selector: Selector) {
+        self.selector = selector;
+    }
+
+    pub fn set_declarations(&mut self, declarations: Vec<Declaration>) {
+        self.declarations = declarations;
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum Selector {
+    TypeSelector(String),
+    ClassSelector(String),
+    IdSelector(String),
+    UnknownSelector,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Declaration {
+    pub property: String,
+    pub value: ComponentValue,
+}
+
+impl Declaration {
+    pub fn new() -> Self {
+        Self {
+            property: String::new(),
+            value: ComponentValue::Ident(String::new()),
+        }
+    }
+
+    pub fn set_property(&mut self, property: String) {
+        self.property = property;
+    }
+
+    pub fn set_value(&mut self, value: ComponentValue) {
+        self.value = value;
+    }
+}
+
+pub type ComponentValue = CssToken;
